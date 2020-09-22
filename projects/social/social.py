23baec3dd8b42dc2a43f6cce294c4ae7d9c684d3
Serina Grill <gril0044@umn.edu>
2020-09-22 00:11:58 -0400

    social exercise complete

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,3 +1,7 @@
+from numpy import random
+from itertools import combinations
+
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -42,11 +46,30 @@ class SocialGraph:
         self.last_id = 0
         self.users = {}
         self.friendships = {}
-        # !!!! IMPLEMENT ME
 
         # Add users
+        for i in range(num_users):
+            self.add_user(i)
 
-        # Create friendships
+        # Get a list of possible friendship combinations
+        combos = list(combinations(list(self.users.keys()), 2))
+
+        # Shuffle list of friendship combinations
+        random.shuffle(combos)
+
+        # Store the number of friendships to create
+        # Divide by 2 because each add_friendship() call will create two friendships
+        num_friendships = int((num_users * avg_friendships) / 2)
+
+        # A list of friendships, of length num_friendships
+        resulting_friendships = combos[:num_friendships]
+
+        # For each tuple friendship
+        for friendship in resulting_friendships:
+
+            # Add it to our social graph
+            self.add_friendship(friendship[0], friendship[1])
+
 
     def get_all_social_paths(self, user_id):
         """
@@ -57,14 +80,85 @@ class SocialGraph:
 
         The key is the friend's ID and the value is the path.
         """
-        visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
-        return visited
+
+        # create a visited dict
+        visited = {}  
+
+        # create a queue
+        queue = []
+
+        # add our user_id to the queue
+        queue.append(user_id)
+
+        # add user_id to dict
+        visited[user_id] = [user_id]
+
+        # while items in our queue
+        while queue:
+
+            # pop off the user at the front of the queue
+            current = queue.pop(0)
+
+            # get user's friends
+            friends = self.friendships[current]
+
+            # for each friend
+            for friend in friends:
+
+                # if we haven't added friend to dictionary
+                if friend not in visited:
+
+                    # add friend to dict with its value being the path
+                    # so far plus this latest node
+                    visited[friend] = visited[current] + [friend]
+
+                    # add the friend to the queue
+                    queue.append(friend)
+
+        # return the dict
+        return visited, len(visited)
+
+# 3. Questions
+# Question 1: To create 100 users with an average of 10 friends each, 
+# how many times would you need to call add_friendship()? Why?
+# Answer 1: 500 times, because each time you call add_friendship() it 
+# creates a friendship for two keys, as it's bidirectional and thus creates 
+# two edges.
+
+# Question 2: If you create 1000 users with an average of 5 random friends 
+# each, what percentage of other users will be in a particular user's extended 
+# social network? What is the average degree of separation between a user 
+# and those in his/her extended network?
+# Answer 2: About 99.5%. There is more room for error in this percentage with
+# a smaller sample size like 10, where connections tend to vary between 8-10.
+
+# 4. Stretch Goals
+# A way to improve the friendship distribution model would be to account for 
+# the fact that once you have one mutual friend you'll have a greater likelihood
+# of having another mutual friend after that. So maybe begin with clusters of 
+# connections/friend groups. 
+
+
+
+
+# Plan for get_all_social_paths()
+# create a queue
+# create a visited dict
+# queue up user-id
+# add visited[user_id] = user_id to dict
+
+# while queue
+    # current = queue.dequeue()
+    # get current's edges 
+    # for each edge
+        # if edge not in dict
+            # create new dict key visited[edge] = [visited[current] + edge]
+            # queue edge up
 
 
 if __name__ == '__main__':
     sg = SocialGraph()
-    sg.populate_graph(10, 2)
+    sg.populate_graph(1000, 5)
     print(sg.friendships)
     connections = sg.get_all_social_paths(1)
     print(connections)
